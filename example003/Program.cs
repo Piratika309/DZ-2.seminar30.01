2f0b31daac1577da8fea1b33bf2f4771062ec5bd
Natali <piratika309@mail.ru>
2023-02-04 22:26:19 +0300

    диапозоны координат по номеру четверти

---
+++ example003/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задать номер четверти, показать диапозоны для возможных координат
+
+Console.WriteLine("Введите номер четверти (I, II, III, IV): ");
+string Number = Console.ReadLine();
+
+if (Number == "I")
+{
+    Console.WriteLine(" В четверти " + Number + " диапозон координат (x>0; y>0) ");
+}
+else if (Number == "II")
+{
+   Console.WriteLine(" В четверти " + Number +  " диапозон координат (x<0; y>0)");
+}
+else if (Number == "III")
+{
+   Console.WriteLine(" В четверти " + Number +  " диапозон координат (x<0; y<0)");
+}
+else if (Number == "IV")
+{
+   Console.WriteLine(" В четверти " + Number +  " диапозон координат (x>0; y<0)");
+}
+else
+{
+     Console.WriteLine("Неверно введен номер четверти, используйте (I, II, III, IV)");
+}
