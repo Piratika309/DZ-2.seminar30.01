96e63f8a71660777d43ef3923fae08ffa21f3d46
Natali <piratika309@mail.ru>
2023-02-04 23:49:11 +0300

    расстояние между точками

---
+++ example004/Program.cs
@@ -0,0 +1,21 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+
+Console.WriteLine ("Введите координаты Х первой точки: ");
+
+double X1 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Введите координаты Y первой точки: ");
+
+double Y1 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Введите координаты Х второй точки: ");
+
+double X2 = double.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine ("Введите координаты Y первой точки: ");
+
+double Y2 = double.Parse(Console.ReadLine() ?? "0");
+
+double Rasst = Math.Sqrt(Math.Pow(X2-X1, 2) + Math.Pow(Y2-Y1, 2));
+
+Console.WriteLine ("Расстояние между точками: " + Rasst);
